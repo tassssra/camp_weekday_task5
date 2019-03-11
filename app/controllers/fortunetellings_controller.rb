9f672231c3c24747889ae8c9f19f76103ca58ea0
yasuda <tasssra@gmail.com>
2019-03-11 15:07:07 +0900

    課題5 完了

--- app/controllers/fortunetellings_controller.rb
+++ app/controllers/fortunetellings_controller.rb
@@ -1,4 +1,14 @@
 class FortunetellingsController < ApplicationController
+  include FortunetellingsHelper, Common
+
   def show
+    @birthday = params[:birthday]
+    @year = @birthday[0, 4]
+    @month = @birthday[4, 2]
+    @day = @birthday[6, 2]
+    @zodiac_sign = decide_zodiac_sign(@month, @day)
+    @age = calculate_age(@birthday)
+    @is_today_birthday = is_today_birthday?(@birthday)
+    @fortune = %w(大吉 中吉 吉 小吉 凶 大凶).sample
   end
 end
